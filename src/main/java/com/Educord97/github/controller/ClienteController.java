7bd857280afe6db943faefb3b0eecdc6da089245
Ryan Santos de Lima <limaryan674@gmail.com>
2021-08-16 19:56:03 -0300

    configure database and jpa

--- src/main/java/com/Educord97/github/controller/ClienteController.java
+++ src/main/java/com/Educord97/github/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.Educord97.github.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +14,13 @@ import com.Educord97.github.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("41-4444-5555");
-		cliente1.setEmail("joao@teste.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("maria");
-		cliente2.setTelefone("11-333-222");
-		cliente2.setEmail("maria@teste.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
