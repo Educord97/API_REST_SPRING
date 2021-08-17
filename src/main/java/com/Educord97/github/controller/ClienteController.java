08fda01a07b2f774f8fc44ed3210e1d9e221cb4b
Ryan Santos de Lima <limaryan674@gmail.com>
2021-08-16 21:56:44 -0300

    spring jpa and repository

--- src/main/java/com/Educord97/github/controller/ClienteController.java
+++ src/main/java/com/Educord97/github/controller/ClienteController.java
@@ -1,26 +1,24 @@
 package com.Educord97.github.controller;
 
-import java.util.Arrays;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Educord97.github.domain.model.Cliente;
+import com.Educord97.github.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
-	
-	@PersistenceContext
-	private EntityManager manager;
+		
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+	  return clienteRepository.findByNomeContaining("z");
 	}
 
 }
---
+++ src/main/java/com/Educord97/github/domain/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.Educord97.github.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Educord97.github.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String Nome);
+	List<Cliente> findByNomeContaining(String Nome);
+}
