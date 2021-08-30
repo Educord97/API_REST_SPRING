6216161ca904b50d6a952b01134f2e2956aa5a8b
Educord97 <eduuhliima97@gmail.com>
2021-08-30 20:15:31 -0300

    DTO

--- src/main/java/com/Educord97/github/api/model/EntregaModel.java
+++ src/main/java/com/Educord97/github/api/model/EntregaModel.java
@@ -1,5 +1,24 @@
 package com.Educord97.github.api.model;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.Educord97.github.domain.model.StatusEntrega;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EntregaModel {
+	
+	private Long id;
+	private String nomeCliente;
+	private DestinatarioModel destinatario;
+	private BigDecimal taxa;
+	private StatusEntrega status;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
+	
 
 }
